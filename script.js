7346bca2c9bcbdbd24fff3baf0ad28a531b017c1
felixrueckl <131243590+felixrueckl@users.noreply.github.com>
2024-12-14 12:53:36 +0100

    Delete script.js
    Removed as moving to Next.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.querySelector('.hamburger-menu');
-    const navMenu = document.querySelector('.nav-menu');
-    
-    hamburger.addEventListener('click', function() {
-        navMenu.classList.toggle('active');
-        this.classList.toggle('active');
-    });
-});
